da57a61f299b11f771ee21effbdd3ec7b4096d20
Khodatskaa <khodatskaalina@gmail.com>
2023-11-07 18:24:01 +0200

    Ex 1 was done

---
+++ cpp_app_18/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+template<typename T>T sum(T n1, T n2)
+{    
+	return n1 + n2;
+}
+template<typename T>T sub(T n1, T n2)
+{   
+	return n1 - n2;
+}
+template<typename T>T mul(T n1, T n2)
+{   
+	return n1 * n2;
+}
+template<typename T>T div(T n1, T n2)
+{    
+	return (n1 == 0 || n2 == 0) ? 0 : n1/n2;
+}
+
+int main()
+{   
+	double n1, n2;   
+	cout << "Enter two numbers: "; 
+	cin >> n1 >> n2;   
+	double (*action[4])(double, double) = {sum, sub, mul, div};   
+	cout << "Enter op (0-3): ";  
+	int op;  
+	cin >> op;  
+	cout << action[op](n1, n2) << endl;    
+	return 0;
+}
